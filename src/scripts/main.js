79eddba0e31ad260c523bf2528cb17443299da01
Bohdan Bilokin <bodya24102001@gmail.com>
2024-01-31 20:27:04 +0200

    abc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@ import { GetBacon } from './utils';
 const baconEl = document.querySelector('.bacon');
 
 GetBacon()
-  .then(res => {
+  .then((res) => {
     const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
     baconEl.innerHTML = markup;
   })
-  .catch(err => (baconEl.innerHTML = err));
+  .catch((err) => (baconEl.innerHTML = err));
