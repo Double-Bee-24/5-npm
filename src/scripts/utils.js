ba4deae913cffd724c00304cdb71f6fa406c7374
Bohdan Bilokin <bodya24102001@gmail.com>
2024-02-05 20:32:11 +0200

    changed place of index.html file

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,5 @@
 export const GetBacon = () => {
-  const body = fetch('https://baconipsum.com/api/?type=all-meat&paras=3').then(
+  const body = fetch("https://baconipsum.com/api/?type=all-meat&paras=3").then(
     (res) => res.json(),
   );
 
